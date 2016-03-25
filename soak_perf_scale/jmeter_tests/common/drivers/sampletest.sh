b580dbeaef70f27042e210d8552ff435efa57528
Ravikumar Venkatesan <ravikumar.venkatesan@hpe.com>
2016-03-25 10:42:35 -0700

    Jmeter sample test file and framework setup

---
+++ soak_perf_scale/jmeter_tests/common/drivers/sampletest.sh
@@ -0,0 +1,1 @@
+/jmeter -n -t $WORKDIR/soak_perf_sacle/jmeter_tests/sample/samplelogintest.jmx -p $WORKDIR/soak_perf_scale/jmeter_tests/common/properties/sample_common.properties -l /home/veera/j  $WORKDIR/sampletestlogin.jtl
