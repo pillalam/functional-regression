c61e5b6f2fbdfda688dc8d4c8a1626d678cd611c
Ravikumar Venkatesan <ravikumar.venkatesan@hpe.com>
2016-04-26 13:47:37 -0700

    Test Selenium file to validate Mvn

---
+++ functional_tests/selenium_tests/src/test/java/com/helion/selenium/StratosConsole/GoogleHomePageTest.java
@@ -0,0 +1,35 @@
+package com.helion.selenium.StratosConsole;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+	
+	private WebDriver driver; 
+	String appURL = "http://google.com";
+
+	@BeforeClass
+	public void testSetUp() {
+		
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
