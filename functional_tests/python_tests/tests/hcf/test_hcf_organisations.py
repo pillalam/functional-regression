cee91230fa5f24ef059610e57b2352393a9e10a8
sunil-mamillapalli <sunil.mamillapalli@hpe.com>
2016-04-26 02:45:18 -0700

    Adds a test suite for hcf functional tests and test organisation scenario

---
+++ functional_tests/python_tests/tests/hcf/test_hcf_organisations.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import re
+import base
+import random
+from utils.hcf import hcf_auth
+from utils.hcf import organisations
+
+
+class TestHcfOrganisations(base.BaseTest):
+
+    """
+    SetupClass prepares the following preconditions for
+    Organisation tests
+    * Connect to the cluster URI target
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestHcfOrganisations, cls).setUpClass()
+
+        # Connect to the cluster URI target
+        hcf_auth.connect_target(cls.cluster_url,
+                                optional_args={'--skip-ssl-validation': ' '})
+        # Loginto Cluster using creds
+        hcf_auth.login(optional_args={'-u': cls.username, '-p': cls.password})
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestHcfOrganisations, cls).tearDownClass()
+        hcf_auth.logout(cls.cluster_url)
+
+    def test_hcf_create_list_delete_org(self):
+        # Create Organisation
+        org_name = 'og_test_org' + str(random.randint(1024, 4096))
+        out, err = organisations.create_org(org_name)
+        self.verify(org_name, out)
+        self.verify("OK", out)
+
+        # List Organisation
+        out, err = organisations.list_orgs()
+        self.verify(org_name, out)
+
+        # Delete Organisation
+        out, err = organisations.delete_org(
+            org_name, input_data=b'yes\n')
+        self.verify("OK", out)
+
+if __name__ == '__main__':
+    base.unittest.main()
---
+++ functional_tests/python_tests/utils/hcf/hcf_auth.py
@@ -0,0 +1,21 @@
+from utils import common
+
+
+def connect_target(cluster_url, optional_args=dict()):
+    out, err = common.frame_command(
+        'hcf', 'api', positional_args=[cluster_url],
+        optional_args=optional_args)
+    return out, err
+
+
+def login(input_data='n\n', optional_args=dict()):
+    out, err = common.frame_command(
+        'hcf', 'login', optional_args=optional_args)
+    return out, err
+
+
+def logout(cluster_url, optional_args=dict()):
+    out, err = common.frame_command(
+        'hcf', 'logout', positional_args=[cluster_url],
+        optional_args=optional_args)
+    return out, err
