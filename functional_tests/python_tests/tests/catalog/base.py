29d6a18327ec1d00e00f1301aca521f2ad0a30e4
mmeen <meenakshim@hpe.com>
2016-05-20 09:00:31 -0700

    Updated Test Cases for Catalog Instances
    Scripted testcases for catalogservice instances for the following services
    1. Havenondemand
    2. Redis
    3. Vertica
    4. Spark
    
    Modified base.py and catalog.conf file.
    
    Also updated catalog_instance.py file.
    
    Reference JIRA ID's (QA-1794), (QA-1795), (QA-1796), (QA-1797)

--- functional_tests/python_tests/tests/catalog/base.py
+++ functional_tests/python_tests/tests/catalog/base.py
@@ -6,17 +6,37 @@ import ConfigParser
 
 class BaseTest(unittest.TestCase):
 
-    BASE_PATH = os.getcwd().split()[0]
-    CONF_FILE = os.environ.get('READ CONFIG FILE', os.path.expanduser(
-        BASE_PATH + '/config/catalog.conf'))
-    Config = ConfigParser.ConfigParser()
-    Config.read(CONF_FILE)
-    CONF_SECTION_CONN = 'catalog-details'
+    @classmethod
+    def get_instance_details(cls):
+        service_details = ['redis-details', 'vertica-details',
+                           'spark-details', 'havenondemand-details']
+        instance_all_details = {}
+        for service in service_details:
+            instance_params = cls.read_from_config(service)
+            instance_all_details[service] = instance_params
+        return instance_all_details
+
+    @classmethod
+    def read_from_config(cls, CONF_SECTION):
+        BASE_PATH = os.getcwd().split()[0]
+        CONF_FILE = os.environ.get('READ CONFIG FILE', os.path.expanduser(
+                                   BASE_PATH + '/config/catalog.conf'))
+        cls.Config = ConfigParser.ConfigParser()
+        cls.Config.read(CONF_FILE)
+        cls.CONF_SECTION_CONN = CONF_SECTION
+        options = cls.Config.options(CONF_SECTION)
+        service_details = {}
+        for option in options:
+            service_details[option] = cls.Config.get(
+                cls.CONF_SECTION_CONN, option)
+        return service_details
 
     @classmethod
     def setUpClass(cls):
+        cls.read_from_config(CONF_SECTION='catalog-details')
         cls.catalog_host = cls.Config.get(
             cls.CONF_SECTION_CONN, 'CATALOG_HOST')
+        cls.get_instance_details()
 
     def verify(self, expression, output):
         if re.search(expression, output):
--- functional_tests/python_tests/tests/catalog/test_catalog_instance.py
+++ functional_tests/python_tests/tests/catalog/test_catalog_instance.py
@@ -20,30 +20,47 @@ class TestCatalogInstance(base.BaseTest):
         super(TestCatalogInstance, cls).tearDownClass()
         pass
 
-    def test_cat_instance(self):
-        instance_id = "1001"
-        instance_name = 'instance_' + str(random.randint(1, 1000))
-        service_id = "hpe-catalog:mysql"
-        version = "5.6"
-        # Create a catalog instance
-        response, instance = catalog_instance.create_instance(
-            self.catalog_host, instance_name, service_id, version)
-        self.assertEqual(response['status'], '200')
-        self.assertEqual(instance['instance_name'], instance_name)
+    def test_create_all_service_instances(self):
+        instance_all_details = self.get_instance_details()
+        # Create Service Catalog Instances
+        for inst in instance_all_details:
+            instance_details = instance_all_details[inst]
+            kwargs = {}
+            if 'kwargs' in instance_details:
+                kwargs['parameters'] = instance_details['kwargs']
+                # Create Catalog Instance
+                response, instance = catalog_instance.create_instance(
+                    self.catalog_host, instance_details['instance_id'],
+                    instance_details['service_id'], instance_details['labels'],
+                    instance_details['version'],
+                    instance_details['description'], **kwargs)
 
-        # List instances
-        response, content = catalog_instance.list_instances(self.catalog_host)
-        self.assertEqual(response['status'], '200')
+                # List instances
+                response, list_instances = catalog_instance.list_instances(
+                    self.catalog_host)
+                self.assertEqual(response['status'], '200')
+                self.assertIn(instance['instance_id'], list_instances)
 
-        # Show details of a catalog instance
-        response, content = catalog_instance.show_instance(
-            self.catalog_host, instance_id)
-        self.assertEqual(response['status'], '200')
+                # Verify details of a catalog instance
+                response, get_instance = catalog_instance.show_instance(
+                    self.catalog_host, instance_id)
+                self.assertEqual(response['status'], '200')
+                self.assertEqual(get_instance['instance_id'],
+                                 instance_details['instance_id'])
+                self.assertEqual(get_instance['service_id'],
+                                 instance_details['service_id'])
+                self.assertEqual(get_instance['labels'],
+                                 instance_details['labels'])
+                self.assertEqual(get_instance['version'],
+                                 instance_details['version'])
+                self.assertEqual(get_instance['description'],
+                                 instance_details['description'])
 
-        # Delete a catalog instance
-        response, instance = catalog_instance.delete_instance(
-            self.catalog_host, instance['instance_id'])
-        self.assertEqual(response['status'], '200')
+                # Delete a catalog instance
+                response, _ = catalog_instance.delete_instance(
+                    self.catalog_host, instance['instance_id'])
+                self.assertEqual(response['status'], '200')
+
 
 if __name__ == '__main__':
     base.unittest.main()
